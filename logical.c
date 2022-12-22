391c8a97292a4f18fae0d7f098d7685d58b40fab
DevilCodder <shivamkrs777@gmail.com>
2022-12-22 16:08:47 +0530

    first commit

---
+++ logical.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int age;
+printf("Enter your age \n");
+scanf("%d",age);
+if ((age <= 70 && age >=18) || (age == 15))
+{
+   printf("you can drive");
+}
+else{
+    printf("you cannot drive.");
+}
+return 0;
+
+}
