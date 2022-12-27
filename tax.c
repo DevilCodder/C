b2326cd382bdf14024593628eb672a4f414efb20
DevilCodder <shivamkrs777@gmail.com>
2022-12-27 10:51:39 +0530

    first commit

--- tax.c
+++ tax.c
@@ -1,28 +1,27 @@
-#include<stdio.h>
-int main(){
-float income;
-float tax = 0;
-printf("What is your Annual Income\n",income);
-scanf("%f",&income);
-if (income<=250000 && income>=500000);
+#include <stdio.h>
+int main()
 {
-   tax = tax + 0.05*(income-250000);
+    float income;
+    float tax = 0;
+    printf("What is your Annual Income\n", income);
+    scanf("%f", &income);
+    if (income <= 250000 && income >= 500000)
+        ;
+    {
+        tax = tax + 0.05 * (income - 250000);
+    }
 
-} 
+    if (income <= 500000 && income >= 1000000);
+    
+    {
+        tax = tax + 0.20 * (income - 250000);
+    }
+    if (income >= 1000000)
+        ;
+    {
+        tax = tax + 0.30 * (income - 250000);
+    }
+    printf("You have to pay a tax amount of:%f", tax);
 
-if (income<=500000 && income>=1000000);
-{
-   tax = tax + 0.20*(income-250000);
-
-}
-if (income>=1000000);
-{
-   tax = tax + 0.30*(income-250000);
-
-}
-  printf("You have to pay a tax amount of:%f",tax);
-
-
-return 0;
-
+    return 0;
 }
